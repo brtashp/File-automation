89a855f6494831cf8614af47f5405c2ca2ca71d4
Britney Shepherd <brta.shp@gmail.com>
2023-07-27 09:55:40 -0400

    one method for cleaning
    not working with this method but want to save anyways

--- v1.py
+++ v1.py
@@ -5,12 +5,6 @@ import os
 
 # create dataframe from excel sheet
 excel_df = pd.read_excel('Test.xlsx')
-#print(excel_df)
-
-# creates new folder, needed to be before calling the path to the Files folder or 
-# else it would store the newly created file in the files folder 
-#renamed_files = 'Renamed Files'
-#os.makedirs(renamed_files)
 
 folder_name = 'Files'
 # below function is used to get the absolute path to a folder (defines the path)
@@ -18,13 +12,38 @@ folder_path = os.path.abspath(folder_name)
 # files_in_folder is the list of files in the folder 
 files_in_folder = os.listdir(folder_name)
 
-print('\n' .join(files_in_folder)) # prints files vertically, not necessary tho
-# prints the names of the files
-# for file_name in files_in_folder: 
-    # print(file_name)
+#print('\n' .join(files_in_folder)) # prints files vertically, not necessary tho
 
-# only puts the three columns we need into selected df
-# need to make a function for later code (unknown spread sheets ect) 
-# defines the new dataframe 
 selected_df = excel_df[['First name', 'Last name', 'UNumber']] #python is case sensative 
 #print(selected_df)
+
+# create the function to identify parts to remove from file name 
+# need to remove the gibberish (pleonasm) 
+# since this will also rename the file, maybe we 'remove' the gibberish, 
+# then search based on the first string found within the first column, 
+# then if we cannot find it, have it search the second column using the first string
+# then repeat the above steps but with the second string, and if there is no second string 
+# then ingore/move the file to the 'Need to be renamed' folder or something?
+
+print(files_in_folder[1])
+file_names = files_in_folder[1]
+
+def extract_names(file_names):
+    parts = file_names.split(" - ")
+    print(type(parts))
+    if len(parts) == 2:
+        names = parts[1].split()
+        print(type(names))
+        print(names)
+        if len(names == 2):
+            name1, name2 = names
+        else:
+            name1, name2 = names[0], None
+    else:
+        name1, name2 = None, None
+    
+    return name1, name2
+
+name1, name2 = extract_names(file_names)
+
+print(name1, name2)
