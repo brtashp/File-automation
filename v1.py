c9afc76166e9179f943902ca8616fbd15ffd63a6
Britney Shepherd <brta.shp@gmail.com>
2023-07-27 09:29:34 -0400

    updating to start clean
    removed code that involved renaming the files since we can merge it later

--- v1.py
+++ v1.py
@@ -28,32 +28,3 @@ print('\n' .join(files_in_folder)) # prints files vertically, not necessary tho
 # defines the new dataframe 
 selected_df = excel_df[['First name', 'Last name', 'UNumber']] #python is case sensative 
 #print(selected_df)
-
-# renaming process 
-old_file_name = files_in_folder[0]
-print(f"old file name is: {old_file_name}")
-new_file_name = 'Whiskers, Cat UNumber.pdf'
-#print(f"new file name is{new_file_name}")
-
-# below function concatenates the file name to the file path, might be where we are having issues
-old_file_path = os.path.join('Files', old_file_name)
-#print( f"old file path is {old_file_path}")
-#new_file_path = os.path.join('Renamed Files', new_file_name)
-new_file_path = os.path.join(os.path.dirname(old_file_path), new_file_name)
-#print(f"new file path is {new_file_path}")
-
-# worked when using old_file_path rather than the name, 
-# when defining the old_file_path, we concatanated the file we wanted and where it was located, see print statement ~ line 40 
-if os.path.exists(old_file_path) and os.path.isfile(old_file_path):
-    # Rename the file
-    # when renaming the file, you put the path to the old file (with name) and path to new file (with name)
-    # NOT using text file names
-    os.rename(old_file_path, new_file_path)
-    print("File renamed successfully.")
-else:
-    print("Original file not found or not accessible.")
-
-# renames the file that was assigned to old_file_name to the text that was assigned as new file name
-#os.rename(old_file_path, new_file_path)
-
-print(files_in_folder)
