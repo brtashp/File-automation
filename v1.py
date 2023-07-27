ad8cde1226174040fa8010cb1d8835ecd1a149d6
Britney Shepherd <brta.shp@gmail.com>
2023-07-27 10:19:07 -0400

    removes gibberish and .filetype
    removes the gibberish and .filetypes

--- v1.py
+++ v1.py
@@ -26,7 +26,7 @@ selected_df = excel_df[['First name', 'Last name', 'UNumber']] #python is case s
 # then repeat the above steps but with the second string, and if there is no second string 
 # then ingore/move the file to the 'Need to be renamed' folder or something?
 
-print(files_in_folder[1])
+#print(files_in_folder[1])
 
 #file_name = files_in_folder[1]
 
@@ -38,15 +38,15 @@ for file_name in files_in_folder:
         names = names_text.split()
 
         if len(names) >= 1:
-
+            # Check and split name2 from the last dot, if it exists
             name1 = names[0]
-            if name1.endswith(".pdf"):
-                name1 = name1[:-4]
-
-            # Check if name2 has ".pdf" extension
             name2 = names[1] if len(names) >= 2 else None
-            if name2 and name2.endswith(".pdf"):
-                name2 = name2[:-4]
+
+            if name1 and "." in name1:
+                name1, _ = name1.rsplit(".", 1)
+
+            if name2 and "." in name2:
+                name2, _ = name2.rsplit(".", 1)
 
             print("Name 1:", name1)
             print("Name 2:", name2)
