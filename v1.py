02b9a2c323812762d41560d2fc51d673e8174067
Britney Shepherd <brta.shp@gmail.com>
2023-07-28 09:01:16 -0400

    need main branch
    need the main branch to start working on looping portion

--- v1.py
+++ v1.py
@@ -17,7 +17,7 @@ files_in_folder = os.listdir(folder_name)
 #print('\n' .join(files_in_folder)) # prints files vertically, not necessary tho
 
 selected_df = excel_df[['First name', 'Last name', 'UNumber']] #python is case sensative 
-print(selected_df)
+#print(selected_df)
 
 # create df to store the output of the named file 
 cleaned_file_names = []
@@ -54,7 +54,7 @@ cleaned_names_df = pd.DataFrame(cleaned_file_names)
 # adds original file name to compare (will be useful later when 
 # comparing to make sure correct file was renamed)
 cleaned_names_df['File Name'] = files_in_folder
-print(cleaned_names_df)
+#print(cleaned_names_df)
 
 # now that we have the file names 'searchable'
 # lets work on the part that compares to excel df so we can find the unumber and rename the file
@@ -65,10 +65,15 @@ print(cleaned_names_df)
 # if there is a match, we rename the file based on 'last, first unumber'
 # if there isnt a match, the file gets moved to a filder named "to rename", or something like that
 
+# shuffle the excel dataframe:
+selected_df = selected_df.sample(frac=1)
+#print(selected_df)
+
 # .isin() function checks if the first part (before the .) is in the second df, returns a boolian 
-matches = cleaned_names_df['First name'].isin(selected_df['First name'])
+matches = cleaned_names_df['Last name'].isin(selected_df['Last name'])
 matching_rows_df = cleaned_names_df[matches]
 print(matching_rows_df)
 print(matches)
 
-
+# looping time 
+# loop 1 - checks if first name matches, if yes then file can be renamed based on the match 
