9a7969b764643642de6f36e66c22f6ff1b875c3a
Britney Shepherd <brta.shp@gmail.com>
2023-08-07 13:16:07 -0400

    fully updated

--- v1.py
+++ v1.py
@@ -24,7 +24,7 @@ excel_df['UNumber'] = excel_df['UNumber'].apply(add_U_if_missing)
 excel_df['UNumber'] = excel_df['UNumber'].apply(remove_dashes)
 
 # Print the modified DataFrame
-print(excel_df)
+#print(excel_df)
 
 folder_name = 'Files'
 # below function is used to get the absolute path to a folder (defines the path)
@@ -38,7 +38,6 @@ selected_df = excel_df[['First name', 'Last name', 'UNumber']] #python is case s
 new_folder_name = 'Redo Names'
 
 # creates the folder with the defined name in 'new_folder_name'
-#os.makedirs(new_folder_name)
 
 cleaned_file_names = []
 
@@ -50,20 +49,26 @@ for file_name in files_in_folder:
         names = names_text.split()
 
         if len(names) >= 1:
-            # Check and split name2 from the last dot, if it exists
-            name1 = names[0]
-            name2 = names[1] if len(names) >= 2 else None
+            # Extract the last name, which might contain the file type
+            last_name = names[-1]
 
-            if name1 and "." in name1:
-                name1, _ = name1.rsplit(".", 1)
+            # Check and split the last name from the last dot
+            if "." in last_name:
+                last_name, file_type = last_name.rsplit(".", 1)
+            else:
+                file_type = None
 
-            if name2 and "." in name2:
-                name2, _ = name2.rsplit(".", 1)
+            # The remaining names are first names
+            first_names = names[:-1]
 
-            entry = {"First name": name1, "Last name": name2}
+            entry = {"First name": ' '.join(first_names), "Last name": last_name, "File type": file_type}
             cleaned_file_names.append(entry)
 
+#print(cleaned_file_names)
+
 cleaned_names_df = pd.DataFrame(cleaned_file_names)
+
+#print(cleaned_names_df)
 
 # adds original file name to compare (will be useful later when 
 # comparing to make sure correct file was renamed)
@@ -75,12 +80,16 @@ selected_df2 = selected_df.sample(frac=1)
 # Perform a left merge to keep all rows from df2 while matching "First name" and "Last name" columns
 merged_df2 = cleaned_names_df.merge(selected_df2, on=['First name', 'Last name'], how='left')
 
-# Reorder the columns to have "First name", "Last name", "UNumber", and "File Names" at the front
-merged_df2 = merged_df2[['First name', 'Last name', 'UNumber', 'File Name']]
+# Reorder the columns to have "First name", "Last name", "UNumber", and "File Type" at the front
+merged_df2 = merged_df2[['First name', 'Last name', 'UNumber', 'File type', 'File Name']]
 
 merged_df2 = merged_df2.astype(str)
+merged_df2['File type'] = merged_df2['File type'].astype(str)
 
 print(merged_df2)
+print(merged_df2.dtypes)
+
+#print(merged_df2)
 # Output the final merged dataframe with "UNumber" added to df2
 #print(merged_df2)
 
@@ -88,7 +97,7 @@ for i in range(len(files_in_folder)):
 
     if merged_df2['UNumber'][i] != 'nan':
         old_file_name = merged_df2.loc[i, 'File Name']
-        new_file_name = merged_df2['Last name'][i] + ", " + merged_df2['First name'][i] + " " + merged_df2['UNumber'][i]
+        new_file_name = merged_df2['Last name'][i] + ", " + merged_df2['First name'][i] + " " + merged_df2['UNumber'][i] + "." + merged_df2['File type'][i]
 
         old_file_path = os.path.join('Files', old_file_name)
         new_file_path = os.path.join(os.path.dirname(old_file_path), new_file_name)
